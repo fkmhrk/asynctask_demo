a871744608c605dd233d12cb73b7f267f347f084
fkm <fkm@fkmsoft.jp>
2012-11-01 04:37:57 +0900

    tried to use executeOnExecutor

--- src/jp/fkmsoft/sample/asynctask/MainActivity.java
+++ src/jp/fkmsoft/sample/asynctask/MainActivity.java
@@ -1,11 +1,33 @@
 package jp.fkmsoft.sample.asynctask;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAXIMUM_POOL_SIZE = 5;
+    private static final long KEEP_ALIVE = 10;
+    private BlockingQueue<Runnable> sWorkQueue = new ArrayBlockingQueue<Runnable>(10);
+    private ThreadFactory sThreadFactory= new ThreadFactory() {
+        private final AtomicInteger mCount = new AtomicInteger(1);
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "AsyncTask #" + mCount.getAndIncrement());
+        }
+    };
+    
+    ThreadPoolExecutor executor1 = new ThreadPoolExecutor(CORE_POOL_SIZE,
+            MAXIMUM_POOL_SIZE, KEEP_ALIVE, TimeUnit.SECONDS, sWorkQueue , sThreadFactory);
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +39,7 @@ public class MainActivity extends Activity {
                 // start 3 task
                 new MyTask().execute(1 * 1000);
                 new MyTask().execute(3 * 1000);
-                new MyTask().execute(2 * 1000);
+                new MyTask().executeOnExecutor(executor1, 2 * 1000);
             }
         };
         
